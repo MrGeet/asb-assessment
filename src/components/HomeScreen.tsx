3e5f4d0f158157ebb775b6a53d9edc9b5760cd42
MrGeet <aldarbatomunkuev@gmail.com>
2022-04-15 19:11:19 +1200

    Use user selector in home screen

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,12 +1,16 @@
 import { Container, Row, Typography } from '../globalStyles';
 import { Form } from './Form';
+import { useSelector } from 'react-redux';
+import { selectUserFullName } from '../features/userSelectors';
 
 export const HomeScreen = () => {
+  const fullName = useSelector(selectUserFullName);
+
   return (
     <Container>
       <Row justify="center" margin="2rem 0">
         <Typography textAlign="center" fontSize="clamp(1rem, 3vw, 2.4rem)">
-          Welcome user
+          Welcome {fullName}
         </Typography>
       </Row>
       <Form />
